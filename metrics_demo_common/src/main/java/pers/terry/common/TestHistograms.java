acd18374b91257f7670151239f20404cbc7a2f31
terry <zhujian19850829@gmail.com>
2017-04-07 17:11:03 +0800

    1

--- metrics_demo_common/src/main/java/pers/terry/common/TestHistograms.java
+++ metrics_demo_common/src/main/java/pers/terry/common/TestHistograms.java
@@ -3,6 +3,9 @@ package pers.terry.common;
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.ScheduledReporter;
+import metrics_influxdb.HttpInfluxdbProtocol;
+import metrics_influxdb.InfluxdbReporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,15 +19,22 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestHistograms {
 
-
     private static final Logger logger = LoggerFactory.getLogger(TestHistograms.class);
 
     private static final MetricRegistry metricRegistry = new MetricRegistry();
     private static final ConsoleReporter consoleReportor = ConsoleReporter.forRegistry(metricRegistry).build();
 
+    public static final ScheduledReporter reporter = InfluxdbReporter.forRegistry(metricRegistry)
+            .protocol(new HttpInfluxdbProtocol("http", "localhost", 8086, null, null, "test"))
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build();
+
+
     private static final Histogram randomNums = metricRegistry.histogram(metricRegistry.name(TestHistograms.class, "random"));
 
     public static void main(String[] args) throws InterruptedException {
+        reporter.start(10, TimeUnit.SECONDS);
         consoleReportor.start(3, TimeUnit.SECONDS);
         Random random = new Random();
         while (true) {
